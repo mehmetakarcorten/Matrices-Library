593639c199a0f18d311e224a7b69f425de9e83e1
Mehmet Akarcorten <97798919+mehmetakarcorten@users.noreply.github.com>
2024-10-07 14:35:26 +0100

    Update Matrices.py
    I have modified the code base to look more pleasing to the eye, added comments and improved the __str__ method to print a much better looking string.

--- Matrices.py
+++ Matrices.py
@@ -1,86 +1,82 @@
 from fractions import Fraction
 
 class Matrix:
-	__slots__ = 'matrix','ord'
+    __slots__ = 'matrix', 'ord'
 
-	def __init__(self,param):
-		self.matrix = param
-		self.ord    = [len(param),len(param[0])] # row major
+    def __init__(self, param):
+        self.matrix = param
+        self.ord = [len(param), len(param[0])]  # row major
 
-	def __str__(self):
-		f = [[Fraction(float(f'{self.matrix[r][c]:.9f}')).limit_denominator() for c in range(self.ord[1])] for r in range(self.ord[0])]
-		x = ""
-		for i in range(self.ord[0]):
-			x += "[\t"
-			for j in range(self.ord[1]):
-				x += f"{f[i][j]}\t"
-			x += "]\n"
-		return x
-		
+    def __str__(self):
+        # Convert matrix values to Fractions with limited denominators
+        fractions_matrix = [[Fraction(f'{self.matrix[r][c]:.9f}').limit_denominator() for c in range(self.ord[1])] for r in range(self.ord[0])]
 
-	def __add__(self,param):
-		__slots__ = "new","param"
-		new = [[] for i in range(self.ord[0])]
-		[new[i].append(self.matrix[i][j]+param.matrix[i][j]) for j in range(self.ord[1]) for i in range(self.ord[0])]
-		return Matrix(new)
+        # Find the maximum length of any string representation in the matrix for better alignment
+        max_len = max(len(str(value)) for row in fractions_matrix for value in row)
 
-	def __sub__(self,param):
-		__slots__ = "new","param"
-		new = [[] for i in range(self.ord[0])]
-		[new[i].append(self.matrix[i][j]-param.matrix[i][j]) for j in range(self.ord[1]) for i in range(self.ord[0])]
-		return Matrix(new)
+        # Build string representation of the matrix with consistent spacing
+        matrix_str = ""
+        for row in fractions_matrix:
+            matrix_str += "[\t" + "\t".join(f"{str(value):>{max_len}}" for value in row) + "\t]\n"
+        return matrix_str
 
-	def __rmul__(self,factor):
-		__slots__ = "new","factor"
-		new = [[] for i in range(self.ord[0])]
-		[new[i].append(self.matrix[i][j]*factor) for j in range(self.ord[1]) for i in range(self.ord[0])]
-		return Matrix(new)
 
-	def __mul__(self,param):
-		__slots__ = "new","param"
-		new = [[[] for __ in range(param.ord[1])] for _ in range(min(self.ord[0],param.ord[0]))]
-		[new[row][column].append(self.matrix[row][r]*param.matrix[r][column]) for r in range(self.ord[1]) for column in range(param.ord[1]) for row in range(self.ord[0])]
-		[exec("new[i][j]=sum(new[i][j])") for j in range(len(new[0])) for i in range(len(new))]
-		return Matrix(new)
+    def __add__(self, other):
+        result = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.ord[1])] for i in range(self.ord[0])]
+        return Matrix(result)
 
-	def determinant(matrix):
-		__slots__ = "new", "matrix"
-		try:
-			new = []
-			for row in range(matrix.ord[0]):
-				for column in range(matrix.ord[1]):
-					new.append(Matrix([[matrix.matrix[r][c] for c in range(matrix.ord[1]) if column != c] for r in range(matrix.ord[0]) if row != r]))
-			if new[0].ord != [1,1]:
-				new = [Matrix.determinant(new[i]) for i in range(len(new))]
-			new = [matrix.matrix[0][i] * new[i] for i in range(matrix.ord[1])]
-			f = 0
-			for i in range(len(new)):
-				if i % 2 == 0:
-					f = f + new[i].matrix[0][0]
-				else:
-					f = f - new[i].matrix[0][0]
-				
-			return Matrix([[f]])
-		except: return matrix
+    def __sub__(self, other):
+        result = [[self.matrix[i][j] - other.matrix[i][j] for j in range(self.ord[1])] for i in range(self.ord[0])]
+        return Matrix(result)
 
-	def transpose(matrix):
-		__slots__ = "new","matrix"
-		new = [[] for i in range(matrix.ord[1])]
-		[new[column].append(matrix.matrix[row][column]) for row in range(matrix.ord[0]) for column in range(matrix.ord[1])]
-		return Matrix(new)
+    def __rmul__(self, factor):
+        result = [[self.matrix[i][j] * factor for j in range(self.ord[1])] for i in range(self.ord[0])]
+        return Matrix(result)
 
-	def inverse(self):
-		__slots__ = "new","lx","coeff","deter"
-		deter = Matrix.determinant(self).matrix[0][0]
-		coeff = 1/deter
-		new = [[] for _ in range(self.ord[0])]
-		for row in range(self.ord[0]):
-			for column in range(self.ord[1]):
-				new[row].append(Matrix.determinant(Matrix([[self.matrix[r][c] for c in range(self.ord[1]) if column != c] for r in range(self.ord[0]) if row != r])).matrix[0][0])
-		new = Matrix.transpose(Matrix(new)).matrix
-		for r in range(len(new)):
-			for c in range(1-(r%2),len(new[0]),2):
-				new[r][c]=new[r][c]*-1
-		lx = [[] for i in range(self.ord[0])]
-		[lx[i].append(new[i][j]*coeff) for j in range(self.ord[1]) for i in range(self.ord[0])]
-		return Matrix(lx)
+    def __mul__(self, other):
+        result = [[sum(self.matrix[row][k] * other.matrix[k][col] for k in range(self.ord[1])) 
+                   for col in range(other.ord[1])] 
+                  for row in range(self.ord[0])]
+        return Matrix(result)
+
+    def determinant(self):
+        if self.ord == [1, 1]:
+            return self  # Base case for recursion
+
+        det_value = 0
+        for col in range(self.ord[1]):
+            # Create submatrix by excluding current row and column
+            submatrix = Matrix([[self.matrix[r][c] for c in range(self.ord[1]) if c != col] 
+                                for r in range(1, self.ord[0])])
+            sign = (-1) ** col
+            det_value += sign * self.matrix[0][col] * submatrix.determinant().matrix[0][0]
+
+        return Matrix([[det_value]])
+
+    def transpose(self):
+        transposed = [[self.matrix[row][col] for row in range(self.ord[0])] for col in range(self.ord[1])]
+        return Matrix(transposed)
+
+    def inverse(self):
+        det = self.determinant().matrix[0][0]
+        if det == 0:
+            raise ValueError("Matrix is singular and cannot be inverted")
+
+        # Calculate matrix of minors
+        minors = [[Matrix([[self.matrix[r][c] for c in range(self.ord[1]) if c != col]
+                           for r in range(self.ord[0]) if r != row]).determinant().matrix[0][0]
+                   for col in range(self.ord[1])] 
+                  for row in range(self.ord[0])]
+
+        # Cofactor matrix
+        cofactors = [[minors[row][col] * (-1 if (row + col) % 2 else 1)
+                      for col in range(self.ord[1])] 
+                     for row in range(self.ord[0])]
+
+        # Transpose cofactor matrix
+        adjugate = Matrix(cofactors).transpose().matrix
+
+        # Multiply adjugate matrix by reciprocal of determinant
+        inverse_matrix = [[adjugate[row][col] / det for col in range(self.ord[1])] for row in range(self.ord[0])]
+        return Matrix(inverse_matrix)
+
